bfd98fadffc7f429cea95d7792e999a4f93a3036
Fizal Sarif <fizal.sarif@gmail.com>
2019-01-04 16:46:38 -0500

    iniital

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.reverse
+  end
+
+  def create
+    Post.create(title: params[:title], body: params[:body], user_id: params[:user_id])
+    redirect_to '/'
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  # /post/:id
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to '/'
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # get 'posts/index'
+  # get 'posts/edit'
+  # get 'posts/update'
+  # get 'posts/destroy'
+  root to: 'posts#index'
+  post '/posts' => 'posts#create'
+  get '/post/:id' => 'posts#destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
