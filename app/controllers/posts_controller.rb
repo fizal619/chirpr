e9b0ac0f9b3e0c4c1aa4c0516cfc2cd2248e6af9
Fizal Sarif <fizal.sarif@gmail.com>
2019-01-07 14:33:46 -0500

    edit logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,22 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all.reverse
+    @new_post = Post.new
   end
 
   def create
-    Post.create(body: params[:body], user_id: params[:user_id])
+    Post.create(post_params)
     redirect_to '/'
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    @post.save
   end
 
   # /post/:id
@@ -20,4 +25,11 @@ class PostsController < ApplicationController
     @post.destroy
     redirect_to '/'
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :user_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   # get 'posts/update'
   # get 'posts/destroy'
   root to: 'posts#index'
-  post '/posts' => 'posts#create'
-  get '/post/:id' => 'posts#destroy'
+  # post '/posts' => 'posts#create'
+  # get '/post/:id' => 'posts#destroy'
+  # put '/post/:id' => 'posts#update'
+
+  resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
